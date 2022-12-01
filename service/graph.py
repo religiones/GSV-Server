89b939db64235bc48207aa33392dbfe4199b18fa
Kronos <1963024305@qq.com>
2022-12-01 21:12:14 +0800

    graph view service finished!

--- service/graph.py
+++ service/graph.py
@@ -9,13 +9,20 @@ class GraphService:
             self.graphDevice = graphDevice
 
     def nodeTypeSelect(self, node):
+        colorArray = ['#f49c84','#099EDA','#FEE301','#ABB7BD','#F4801F','#D6C223',
+                '#D75D73','#E0592B', '#58B7B3', '#68bb8c','#3F3B6C']
         if "Domain" in node["id"]:
             return {
                 "id": node["id"],
                 "name": node["name"],
-                "type": "Domain",
+                "nodeType": "Domain",
+                "style": {
+                    "fill": "#6ECCAF",
+                    "lineWidth": 1,
+                    # "opacity": 0.6
+                },
                 "community": node["community"],
-                "properties": {
+                "donutAttrs": {
                     "porn": strtobool(node["porn"]),
                     "gambling": strtobool(node["gambling"]),
                     "fraud": strtobool(node["fraud"]),
@@ -25,15 +32,31 @@ class GraphService:
                     "trading": strtobool(node["trading"]),
                     "pay": strtobool(node["pay"]),
                     "other": strtobool(node["other"])
+                },
+                "donutColorMap": {
+                    "porn": colorArray[0],
+                    "gambling": colorArray[1],
+                    "fraud": colorArray[2],
+                    "drug": colorArray[3],
+                    "gun": colorArray[4],
+                    "hacker": colorArray[5],
+                    "trading": colorArray[6],
+                    "pay": colorArray[7],
+                    "other": colorArray[8]
                 }
             }
         elif "IP" in node["id"]:
             return {
                 "id": node["id"],
                 "name": node["name"],
-                "type": "IP",
+                "nodeType": "IP",
+                "style": {
+                    "fill": "#FF6D28",
+                    "lineWidth": 1,
+                    # "opacity": 0.6
+                },
                 "community": node["community"],
-                "properties": {
+                "attrs": {
                     "ipc_id": node["ipc_id"],
                     "ipc": node["ipc"]
                 }
@@ -42,13 +65,18 @@ class GraphService:
             return {
                 "id": node["id"],
                 "name": node["name"],
-                "type": "Cert",
+                "nodeType": "Cert",
+                "style": {
+                    "fill": "#5C2E7E",
+                    "lineWidth": 1,
+                    # "opacity": 0.6
+                },
                 "community": node["community"]
             }
 
     def getGraphByCommunity(self, id):
         node_sql = "match (n) where n.community ="+id+" return n"
-        edge_sql = "match (n)-[r]-(m) where n.community="+id+" and m.community="+id+" return n.id,m.id"
+        edge_sql = "match (n)-[r]->(m) where n.community="+id+" and m.community="+id+" return n.id,m.id"
         res_node = self.graphDevice.run(node_sql)
         res_edge = self.graphDevice.run(edge_sql)
         node_list = []
