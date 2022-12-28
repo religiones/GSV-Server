6ce8dc5f76caddb92a34251ba105ba699231f959
Kronos <1963024305@qq.com>
2022-12-28 17:41:01 +0800

    2

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ app = Flask(__name__)
 CORS(app, resources=r'/*')  # 跨域
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "chinavis"))
 
-num_walks = 80    # 序列数量
-walk_length = 10  # 序列长度
-workers = 4
 # service
 communityService = CommunityService(graphDevice=graph)
 graphService = GraphService(graphDevice=graph)
@@ -55,39 +52,17 @@ def get_neighbors():
     else:
         return  "no neighbor data"
 
-@app.route('/api/getGraphEmbedding', methods=["GET", "POST"])
-def get_graph_embedding():
-    node_sql = "match (n) where n.community=1782816 return n.id"
-    edge_sql = "match (n)-[r]-(m) where n.community=1782816 and m.community=1782816 return n.id,m.id"
-    res_node = graph.run(node_sql).to_ndarray()
-    res_edge = graph.run(edge_sql).to_ndarray()
-    node_list = []
-    for node in res_node:
-        node_list.append((node[0]))
-    edge_list = [tuple(y) for y in res_edge]
-    # create graph
-    G = nx.DiGraph()
-    G.add_edges_from(edge_list)
-    G.add_nodes_from(node_list)
-    # RandomWaker产生序列
-    rw = RandomWalker(G, p=1, q=1, use_rejection_sampling=0)
-    rw.preprocess_transition_probs()
-    sentences = rw.simulate_walks(num_walks=num_walks, walk_length=walk_length, workers=workers, verbose=1)
-    model = Word2Vec(sentences=sentences,
-                     vector_size=128,
-                     min_count=5,
-                     sg=1,
-                     hs=0,
-                     workers=workers,
-                     window=5,
-                     epochs=3)
-    embedding_list = []
-    for word in G.nodes():
-        embedding_list.append(model.wv[word])
-    embedding_list = np.array(embedding_list).flatten().tolist()
-    embedding_list_pad = np.pad(embedding_list, (0, 55936 - len(embedding_list)), 'constant').tolist()
-    # nodes_json = {"nodes":G.nodes(),"edges":G.edges(),"community_id":"1782816"}
-    return {"embedding":embedding_list_pad,"community_id":"1782816"}
+@app.route('/api/similarity', methods=["GET", "POST"])
+def get_similarity():
+    val = request.get_json()
+    source = val["source"]
+    target = val["target"]
+    max = val["max"]
+    res = communityService.get_graph_embedding(target)
+    if res != None:
+        return res
+    else:
+        return "no similarity data"
 
 if __name__ == '__main__':
     app.run()
--- service/community.py
+++ service/community.py
@@ -1,6 +1,12 @@
+import networkx as nx
+import numpy as np
+from gensim.models import Word2Vec
 from py2neo import *
 import json
 import os
+
+from model.RandomWalker import RandomWalker
+
 path = os.getcwd()  #获取当前路径
 
 class CommunityService:
@@ -15,3 +21,42 @@ class CommunityService:
         with open(path+"\dbData\community_node.json",'r', encoding='utf-8') as fp:
             data = json.load(fp)
             return data
+
+    "get graph embedding"
+    def get_graph_embedding(self, id):
+        num_walks = 80  # 序列数量
+        walk_length = 10  # 序列长度
+        workers = 4
+        node_sql = "match (n) where n.community="+id+" return n.id"
+        edge_sql = "match (n)-[r]->(m) where n.community="+id+" and m.community="+id+" return n.id,m.id"
+        res_node = self.graphDevice.run(node_sql).to_ndarray()
+        res_edge = self.graphDevice.run(edge_sql).to_ndarray()
+        node_list = []
+        for node in res_node:
+            node_list.append((node[0]))
+        edge_list = [tuple(y) for y in res_edge]
+        # create graph
+        G = nx.DiGraph()
+        G.add_edges_from(edge_list)
+        G.add_nodes_from(node_list)
+        # RandomWaker产生序列
+        rw = RandomWalker(G, p=1, q=1, use_rejection_sampling=0)
+        rw.preprocess_transition_probs()
+        sentences = rw.simulate_walks(num_walks=num_walks, walk_length=walk_length, workers=workers, verbose=1)
+        model = Word2Vec(sentences=sentences,
+                         vector_size=128,
+                         min_count=5,
+                         sg=1,
+                         hs=0,
+                         workers=workers,
+                         window=5,
+                         epochs=3)
+        embedding_list = []
+        for word in G.nodes():
+            embedding_list.append(model.wv[word].tolist())
+        return {"embedding": embedding_list, "community_id": id}
+
+    "get similarity community"
+    def getSimilarityCommunity(self, target, source, max):
+
+        return None
